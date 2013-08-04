8baa3099d878bc5ee497418c2d5538ec64a55302
Kanishak Kataria <kanishak3346@gmail.com>
2013-08-04 10:28:01 +0530

    added reco v6

--- web/utils.php
+++ web/utils.php
@@ -97,7 +97,7 @@
 			$one = "1";
 			$check_reco = "SELECT * FROM `LikedLinks` WHERE YourID='".$_SESSION['fb_id']."' AND Links='".$result['clickurl']."' AND Reco = 1";
 			$result2 = mysql_query($check_reco,$con);
-			if(!$result2){
+			if(mysql_num_rows($result2)){
 				$result['self_reco'] = false;
 				echo mysql_errno($con).": ".mysql_error($con)."\n";
 			}
