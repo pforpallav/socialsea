3382c084aecf3cdb8869f4a0c32cf9518ce1379f
Pallav <pforpallav@gmail.com>
2013-08-05 04:07:14 +0530

    Updated index.php

--- web/forward.php
+++ web/forward.php
@@ -9,14 +9,18 @@ require('utils.php');
 $forward_url = $_GET['url'];
 echo $_SESSION['fb_id'];
 echo $forward_url;
-$check_sql = "IF EXISTS (SELECT count(*) FROM LikedLinks WHERE YourID=".$_SESSION['fb_id']."AND Links=$forward_url)";
+$check_sql = "SELECT * FROM LikedLinks WHERE YourID=".$_SESSION['fb_id']."AND Links=$forward_url";
 $check_count = run_query($check_sql);
 echo $check_count;
-if($check_count!=0){
+if(mysql_num_rows($check_count)==0){
 	$link_sql = "INSERT INTO LikedLinks (YourID, Links) VALUES (".$_SESSION['fb_id'].",$forward_url)";
 	run_query($link_sql);
 ?><script>alert("Added entry!! <?php echo $check_count;?>")</script><?php
 }
+else{
+	<script>alert(" entry already present!!")</script>
+}
+
 
 //header("Location: ".$forward_url);
 ?>
