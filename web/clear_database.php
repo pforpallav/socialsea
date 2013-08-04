0126fa8713f745b35b3130e81309a7c41630a805
Pallav <pforpallav@gmail.com>
2013-08-05 04:29:57 +0530

    Updated index.php

---
+++ web/clear_database.php
@@ -0,0 +1,12 @@
+<?php
+//phpinfo();
+session_start();
+ini_set('display_errors',1);
+ini_set('display_startup_errors',1);
+error_reporting(-1);
+require('utils.php');
+
+$delete_sql = "DELETE * FROM `LikedLinks`";
+$check_count = run_query($delete_sql);
+
+?>
