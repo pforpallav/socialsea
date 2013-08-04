01ef6f963f1532e632f46dbd490b38a32ce3c278
Pallav <pforpallav@gmail.com>
2013-08-04 20:05:26 +0530

    hahaha

--- web/search.php
+++ web/search.php
@@ -1,10 +1,9 @@
 <?php
-echo "hahahahha";
-phpinfo();
+//phpinfo();
 ini_set('display_errors',1);
 ini_set('display_startup_errors',1);
 error_reporting(-1);
-require('oauth/OAuth.php');
+//require('oauth/OAuth.php');
 
 
 $cc_key  = "dj0yJmk9YWF3ODdGNWZPYjg2JmQ9WVdrOWVsWlZNRk5KTldFbWNHbzlNVEEyTURFNU1qWXkmcz1jb25zdW1lcnNlY3JldCZ4PTUz";
@@ -13,17 +12,21 @@ $url = "http://yboss.yahooapis.com/ysearch/web";
 $args = array();
 $args["q"] = "dota 2";
 $args["format"] = "json";
+
+$req_url = 'https://fireeagle.yahooapis.com/oauth/request_token';
+$authurl = 'https://fireeagle.yahoo.net/oauth/authorize';
+$acc_url = 'https://fireeagle.yahooapis.com/oauth/access_token';
+$api_url = 'https://fireeagle.yahooapis.com/api/0.1';
+
+try {
+  $oauth = new OAuth($cc_key, $cc_secret, OAUTH_SIG_METHOD_HMACSHA1, OAUTH_AUTH_TYPE_URI);
+  $oauth->enableDebug();
+  $oauth->setToken(NULL);
+  $oauth->fetch($url,$args);
+  $json = json_decode($oauth->getLastResponse(),true);
+} catch(OAuthException $E) {
+  print_r($E);
+}
  
-$consumer = new OAuthConsumer($cc_key, $cc_secret);
-$request = OAuthRequest::from_consumer_and_token($consumer, NULL,"GET", $url, $args);
-$request->sign_request(new OAuthSignatureMethod_HMAC_SHA1(), $consumer, NULL);
-$url = sprintf("%s?%s", $url, OAuthUtil::build_http_query($args));
-$ch = curl_init();
-$headers = array($request->to_header());
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_URL, $url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-$rsp = curl_exec($ch);
-$results = json_decode($rsp);
-print_r($results);
+print_r($json);
 ?>
