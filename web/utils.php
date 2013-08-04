e10ec0a341aafcfef1a521645769d8e9942158cb
Kanishak Kataria <kanishak3346@gmail.com>
2013-08-04 10:00:36 +0530

    added reco v5

--- web/utils.php
+++ web/utils.php
@@ -95,7 +95,7 @@
 				}
 			}
 			$one = "1";
-			$check_reco = "SELECT * FROM `LikedLinks` WHERE YourID='".$_SESSION['fb_id']."' AND Links='".$result['clickurl']."' AND Reco = $one";
+			$check_reco = "SELECT * FROM `LikedLinks` WHERE YourID='".$_SESSION['fb_id']."' AND Links='".$result['clickurl']."' AND Reco = 1";
 			$result2 = mysql_query($check_reco,$con);
 			if(!$result2){
 				$result['self_reco'] = false;
