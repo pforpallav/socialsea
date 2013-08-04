ac5ca489cf76f12cc599959ccf2b7a0170715855
Kanishak Kataria <kanishak3346@gmail.com>
2013-08-04 10:22:12 +0530

    added reco v6

--- web/reco.php
+++ web/reco.php
@@ -8,13 +8,14 @@ require('utils.php');
 $forward_url = $_GET['url'];
 $check_sql = "SELECT * FROM `LikedLinks` WHERE YourID='".$_SESSION['fb_id']."' AND Links='".$forward_url."'";
 $check_count = run_query($check_sql);
-if(!$check_count){
+if(mysql_num_rows($check_count)==0){
 	echo 'not present';
 	$link_sql = "INSERT INTO `LikedLinks` (YourID,Links,ViewCount,Reco) VALUES ('".$_SESSION['fb_id']."','".$forward_url."',1,1)";
 	run_query($link_sql);
 }
 else
 {
+	echo 'present';
 	$link_sql = "UPDATE `LikedLinks` SET Reco=1 WHERE YourID='".$_SESSION['fb_id']."' AND Links='".$forward_url."'";
 	run_query($link_sql);
 }
