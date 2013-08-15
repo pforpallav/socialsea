767b4c51743cd75df4801d13d0820915e4015195
Pallav <pforpallav@gmail.com>
2013-08-15 15:27:40 +0530

    Refined CSS

--- web/footer.php
+++ web/footer.php
@@ -1,9 +1,7 @@
-<div class='yui3-g-r footer'>
-	<div class='yui3-u-5-6' style="padding-top: 35px;padding-left: 90px;font-size: 14px;width:77%;">
-		Pallav, Kanishak, Rohit
-	</div>
-	<div class='yui3-u-1-6' >
-			<img src="yboss.png" style="padding-bottom: 11;height:auto;width: 25%;"/>
-			<img src="yui.jpg" style="height:auto;width: 20%;" />
+<div class="yui3-g-r footer">
+	<div class="yui3-u-5-6" style="/* padding-top: 35px; */padding-left: 20px;font-size: .8em;width:77%;">
+		Pallav, Kanishak, Rohit <br>
+      	<img src="yboss.png" style="float: left;/* padding-bottom: 163; */height:auto;width: 6%;">
+      	<img src="yui.jpg" style="margin-top: -19;float: left;height:auto;width: 5%;">
 	</div>
 </div>
--- web/index.php
+++ web/index.php
@@ -23,76 +23,75 @@ Social Sea
             </form>
         </div>
 
-                <?php 
-                    if(isset($_GET['q']))
-                    {
-                ?>
-                <div class='yui3-g-r result-div'>
-                <?php
-                        $json=searchBOSS($_GET['q']);
-                        $json=process_json($json);
-
-                        //var_dump($json);
-                        $results = $json;//["bossresponse"]["web"]["results"];
-                        foreach ($results as $key) {
-                            //echo $key['dispurl'];?>
-                            <!-- repeat this block -->
-                            <div class="yui3-u-3-5 results">
-                                <div class="title" style="color:#00f;">
-                                    <h3><a href="forward.php?url=<?php echo $key['clickurl']; ?>"><?php echo $key['title']; ?></a>
-                                    <span style="color: darkgreen;font-style: italic;font-size: 12px;float: right;text-align: right;">
-                                        <p style="margin: 5px 0px -5px 0px;">
-                                        <?php 
-                                         if($key['count']>0){
-                                         	echo $key['count'].'  of your friends visited this';
-                                         }?> 
-                                        </p>
-                                        <p style="color: darkred;"> 
-                                        <?php 
-                                         if($key['reco_count']>0){
-                                            echo $key['reco_count'].' recommended this';
-                                         }
-                                         if(!$key['self_reco']){ ?>
-                                            <a href="reco.php?url=<?php echo $key['clickurl']; ?>&curr=<?php echo 'https://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']?>">Recommend</a>
-                                        <?php } else {?> 
-                                            <a href="dereco.php?url=<?php echo $key['clickurl']; ?>&curr=<?php echo 'https://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']?>">Un-Recommend</a>
-                                        <?php }?>
-                                        </p>
-                                    </span>
-                                    </h3>
-                                </div>
-                                <div class="dispurl" style="color:#3b3;margin-top:-18px;">
-                                    <?php echo $key['dispurl']; ?>
-                                </div>
-                                <div class="content">
-                                    <?php echo $key['abstract']; ?>
-                                </div>
-                            </div>
-                            <?php
-
-                        }
-                    }
-                ?>
-
-                </div>
-        <?php }
-            else{ 
-                $config = array(
-                    'appId' => '473716305986228',
-                    'secret' => '833d11c4693957b672518cdb8319f924',
-                );
-                $facebook = new Facebook($config);
-                $params = array('redirect_uri' => 'https://socialsearch.cloudcontrolled.com');
-
-                $login_url = $facebook->getLoginUrl();
+            <?php 
+                if(isset($_GET['q']))
+                {
             ?>
-            <div class='yui3-g-r' style="text-align:center;padding-top:100px">
-            <a href="<?php echo $login_url ?>"><img src="login_1.png" style="height:172.5px;width:360px"/></a>
+            <div class='yui3-g-r result-div'>
+            <?php
+                    $json=searchBOSS($_GET['q']);
+                    $json=process_json($json);
+
+                    //var_dump($json);
+                    $results = $json;//["bossresponse"]["web"]["results"];
+                    foreach ($results as $key) {
+                        //echo $key['dispurl'];?>
+                        <!-- repeat this block -->
+                        <div class="yui3-u-3-5 results">
+                            <div class="title" style="color:#00f;">
+                                <h3><a href="forward.php?url=<?php echo $key['clickurl']; ?>"><?php echo $key['title']; ?></a>
+                                <span style="color: darkgreen;font-style: italic;font-size: 12px;float: right;text-align: right;">
+                                    <p style="margin: 5px 0px -5px 0px;">
+                                    <?php 
+                                     if($key['count']>0){
+                                     	echo $key['count'].'  of your friends visited this';
+                                     }?> 
+                                    </p>
+                                    <p style="color: darkred;"> 
+                                    <?php 
+                                     if($key['reco_count']>0){
+                                        echo $key['reco_count'].' recommended this';
+                                     }
+                                     if(!$key['self_reco']){ ?>
+                                        <a href="reco.php?url=<?php echo $key['clickurl']; ?>&curr=<?php echo 'https://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']?>">Recommend</a>
+                                    <?php } else {?> 
+                                        <a href="dereco.php?url=<?php echo $key['clickurl']; ?>&curr=<?php echo 'https://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']?>">Un-Recommend</a>
+                                    <?php }?>
+                                    </p>
+                                </span>
+                                </h3>
+                            </div>
+                            <div class="dispurl" style="color:#3b3;margin-top:-18px;">
+                                <?php echo $key['dispurl']; ?>
+                            </div>
+                            <div class="content">
+                                <?php echo $key['abstract']; ?>
+                            </div>
+                        </div>
+                        <?php
+
+                    }
+                }
+            ?>
+
             </div>
-        <?php } ?>
+                <?php }
+                    else{ 
+                        $config = array(
+                            'appId' => '473716305986228',
+                            'secret' => '833d11c4693957b672518cdb8319f924',
+                        );
+                        $facebook = new Facebook($config);
+                        $params = array('redirect_uri' => 'https://socialsearch.cloudcontrolled.com');
 
-    </div>
+                        $login_url = $facebook->getLoginUrl();
+                    ?>
+                    <div class='yui3-g-r' style="text-align:center;padding-top:100px">
+                    <a href="<?php echo $login_url ?>"><img src="login_1.png" style="height:172.5px;width:360px"/></a>
+                    </div>
+                <?php } ?>
 
+            </div>
     <?php include "footer.php"; ?>
 </body>
 </html>
